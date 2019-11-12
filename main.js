242fd174a5188518806cdab805dd8d6400d25658
Kisan Patel <kpate262@uic.edu>
2019-11-12 11:30:04 -0600

    Now earth, asteroid, and ufo positions are chosen at random.
    Asteroid and ufo positions change everytime they touch left side of the screen or the Earth/player.

--- main.js
+++ main.js
@@ -52,7 +52,47 @@ var hourOffset = '0';
 
 var score = 0;
 
-function init(){
+const getrandomNum = () => {
+  var min = 50;
+  var max = 551
+  var randomNum = Math.floor(Math.random() * (max - min + 1) ) + min;
+
+  if (randomNum >= 50 && randomNum < 100){
+    return 50;
+  }
+  else if (randomNum >= 100 && randomNum < 150){
+    return 100;
+  }
+  else if (randomNum >= 150 && randomNum < 200){
+    return 150;
+  }
+  else if (randomNum >= 200 && randomNum < 250){
+    return 200;
+  }
+  else if (randomNum >= 250 && randomNum < 300){
+    return 250;
+  }
+  else if (randomNum >= 300 && randomNum < 350){
+    return 300;
+  }
+  else if (randomNum >= 350 && randomNum < 400){
+    return 350;
+  }
+  else if (randomNum >= 400 && randomNum < 450){
+    return 400;
+  }
+  else if (randomNum >= 450 && randomNum < 500){
+    return 450;
+  }
+  else if (randomNum >= 500 && randomNum < 550){
+    return 500;
+  }
+  else if (randomNum >= 550 && randomNum < 600){
+    return 550;
+  }
+}
+
+const init = () => {
   c = document.getElementById("animationCanvas");
   ctx = c.getContext("2d");
   score = 0;
@@ -65,13 +105,17 @@ function init(){
   minute = 0;
   hour = 0;
   lvl = 0;
-  numLives = 1;
+  numLives = 5;
+  astx = 600;
+  asty = getrandomNum();
+  ufox = 600;
+  ufoy = getrandomNum();
   earthx = 0;
-  earthy = 250;
+  earthy = getrandomNum();
   window.requestAnimationFrame(draw);
 }
 
-function gameOver(){
+const gameOver = () => {
   ctx.font = "50px Comic Sans MS";
   ctx.fillStyle = "red";
   ctx.fillText('Game Over', c.width/4, c.height/2);
@@ -93,7 +137,9 @@ function gameOver(){
   ctx = null;
 }
 
-function setTimeText(){
+
+
+const setTimeText = () => {
   if (seconds >= 0 && seconds <= 9){
     secondOffset = '0' + seconds.toString();
   }
@@ -133,7 +179,8 @@ function setTimeText(){
 
 }
 
-function getTime(){
+
+const getTime = () => {
   seconds += 1;
   setTimeText();
 }
@@ -145,7 +192,25 @@ $(document).keydown(function(e){
     console.log(earthy);
 });
 
-function draw() {
+const draw = () => {
+  if(lvl === 4){
+    astSpeed = 2;
+    ufoSpeed = 2;
+  }
+  else if (lvl === 6){
+    astSpeed = 3;
+    ufoSpeed = 3;
+  }
+  else if (lvl === 8){
+    astSpeed = 4;
+    ufoSpeed = 4;
+  }
+  else if (lvl === 10){
+    astSpeed = 5;
+    ufoSpeed = 5;
+  }
+
+
   if(numLives != 0){
     astx -= astSpeed*(lvl + astSpeed);
     ufox -= ufoSpeed*(lvl + ufoSpeed);
@@ -154,21 +219,37 @@ function draw() {
     gameOver();
   }
 
-  if(earthy === asty && (astx >= -75 && astx <= 45)){
+  if(earthy === asty && (astx >= -35 && astx <= 45)){
     astx = 600;
+    asty = getrandomNum();
+    if(asty === ufoy && Math.abs(astx - ufox) > 100){
+      asty = getrandomNum();
+    }
     numLives--;
   }
 
   if(earthy === ufoy && (ufox >= -45 && ufox <= 45)){
     ufox = 600;
+    if(asty === ufoy && Math.abs(astx - ufox) > 100){
+      ufoy = getrandomNum();
+    }
+    ufoy = getrandomNum();
     score++;
   }
 
   if(astx < -80){
+    asty = getrandomNum();
+    if(asty === ufoy && Math.abs(astx - ufox) > 100){
+      asty = getrandomNum();
+    }
     astx = 600;
   }
 
   if(ufox < -50){
+    ufoy = getrandomNum();
+    if(asty === ufoy && Math.abs(astx - ufox) > 100){
+      ufoy = getrandomNum();
+    }
     ufox = 600;
   }
 
